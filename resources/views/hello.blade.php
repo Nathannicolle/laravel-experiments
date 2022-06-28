bb033d638abe433d5289cee91ad736546f62d46f
Nathannicolle <nathan-nicolle@orange.fr>
2022-06-28 17:05:38 +0200

    first route and view + some funny css

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello world</title>
+    <style>
+        body {
+            background-color: #1a202c;
+            color: white;
+            font-family: 'Nunito', sans-serif;
+            text-align: center;
+            animation: 5s transformation infinite;
+        }
+
+        @keyframes transformation {
+            0% {
+                font-size: 1em;
+
+            }
+
+            50% {
+                font-size: 1.5em;
+                color: aqua;
+            }
+        }
+    </style>
+</head>
+<body>
+    <h1>Hello world !</h1>
+</body>
+</html>
